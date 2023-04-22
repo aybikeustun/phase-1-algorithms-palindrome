c01a4133d9bd641ef5c9f86c7eb05e1aa4baef85
aybikeustun <aybikeustun@gmail.com>
2023-04-22 10:49:21 +0300

    done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+  return word.split("").reverse().join("");
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word) {
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
+}
+
 
 /*
   Add written explanation of your solution here
@@ -20,6 +23,23 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+
+  console.log("");
 }
 
 module.exports = isPalindrome;
